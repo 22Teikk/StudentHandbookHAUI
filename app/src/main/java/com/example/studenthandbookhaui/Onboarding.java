054d5774da67fd5d89a2d1ce012c2856788c9cd8
22Teikk <106013671+22Teikk@users.noreply.github.com>
2023-06-10 19:12:52 +0700

    Done Onboarding

--- app/src/main/java/com/example/studenthandbookhaui/Onboarding.java
+++ app/src/main/java/com/example/studenthandbookhaui/Onboarding.java
@@ -3,19 +3,24 @@ package com.example.studenthandbookhaui;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class Onboarding extends AppCompatActivity {
-
+    Button btnStarted;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_onboarding);
+        btnStarted = findViewById(R.id.btnStarted);
+        btnStarted.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(Onboarding.this, HomePage.class);
+                startActivity(intent);
+            }
+        });
     }
 
-    public void getStarted(View v){
-        Intent intent = new Intent(this, HomePage.class);
-        startActivity(intent);
-    }
 }
